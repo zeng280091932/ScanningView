1364de04b0ad1d73505bc0b024d1cc262f95f84b
zengjiantao <280091932@qq.com>
2020-09-29 16:41:45 +0800

    完成发光容器示例

--- app/src/main/java/com/beauney/scanning/ScanningLayoutDemoActivity.java
+++ app/src/main/java/com/beauney/scanning/ScanningLayoutDemoActivity.java
@@ -1,6 +1,9 @@
 package com.beauney.scanning;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.beauney.scanning.library.ScanningLinearLayout;
 
 import androidx.annotation.Nullable;
 
@@ -9,9 +12,21 @@ import androidx.annotation.Nullable;
  * @since 2020-09-28
  */
 public class ScanningLayoutDemoActivity extends BaseActivity {
+    private ScanningLinearLayout mScanningLayout;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanning_layout_demo);
+
+        mScanningLayout = findViewById(R.id.scanning_container);
+    }
+
+    public void startScanning(View view) {
+        mScanningLayout.start();
+    }
+
+    public void stopScanning(View view) {
+        mScanningLayout.stop();
     }
 }
